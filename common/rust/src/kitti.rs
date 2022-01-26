2dd8bdffab05476d5cb3cfda09ef9fa285c36b2b
malwrar <malwrar@gmail.com>
2022-01-25 22:15:22 -0800

    Add beginnings of a kitti raw dataset parser.

--- common/rust/src/kitti.rs
+++ common/rust/src/kitti.rs
@@ -10,147 +10,202 @@
 //!   booktitle = {Conference on Computer Vision and Pattern Recognition (CVPR)},
 //!   year = {2012}
 
+use chrono::prelude::*;
+
 use std::cmp::Ordering;
 use std::convert::TryInto;
-use std::io::{BufReader, Read};
-use std::fs::File;
+use std::io::{BufReader, BufRead};
+use std::path::PathBuf;
+use std::fs;
+use std::io;
 
 use nalgebra as na;
 use regex::Regex;
 use zip::read::{ZipArchive, ZipFile};
 
-lazy_static::lazy_static! {
-    // ex: dataset/sequences/21/velodyne/002552.bin
-    static ref ODOMETRY_VELODYNE_FNAME_META_EXTACTOR: Regex = Regex::new(r"(?x)
-dataset/sequences/
-(?P<sequence_num>\d+)/
-velodyne/
-(?P<capture_num>\d+)\.bin").unwrap();
-}
+//lazy_static::lazy_static! {
+    //// ex: dataset/sequences/21/velodyne/002552.bin
+    //static ref ODOMETRY_VELODYNE_FNAME_META_EXTACTOR: Regex = Regex::new(r"(?x)
+//dataset/sequences/
+//(?P<sequence_num>\d+)/
+//velodyne/
+//(?P<capture_num>\d+)\.bin").unwrap();
+//}
 
-///
+/////
+//#[derive(Debug)]
+//pub struct OdometryCapture {
+    //pub sequence_num: u32,
+    //pub capture_num: u32,
+    //pub points: Vec<na::Vector3<f32>>,  // TODO: determine the coordinate system used by kitti
+//}
+
+///// The  total kitti dataset is over 100gb, so I wanted to ensure this extractor could work w/ a
+///// partial copy. Multiple optional parameters seems to be best addressed with a builder, so here
+///// it is.
+//pub struct OdometryDatasetExtractorBuilder {
+    //velodyne_archive: Option<ZipArchive<BufReader<File>>>,
+//}
+
+//impl OdometryDatasetExtractorBuilder {
+    //pub fn velodyne_zip(mut self, filepath: &str) -> Self {
+        //let file = File::open(filepath).expect("Unable to locate velodyne archive");
+        //let file_reader = BufReader::new(file);
+
+        //self.velodyne_archive = match ZipArchive::new(file_reader) {
+            //Ok(a) => Some(a),
+            //_ => {
+                //log::warn!("Couldn't open provided velodyne archive @ {}",
+                    //filepath);
+                //None
+            //}
+        //};
+
+        //self
+    //}
+
+    //pub fn build(self) -> OdometryDatasetExtractor {
+        //OdometryDatasetExtractor {
+            //velodyne_archive: self.velodyne_archive.expect("Couldn't find velodyne file!"),
+        //}
+    //}
+//}
+
+//pub struct OdometryDatasetExtractor {
+    //velodyne_archive: ZipArchive<BufReader<File>>,
+//}
+
+//impl OdometryDatasetExtractor {
+    //pub fn construct() -> OdometryDatasetExtractorBuilder {
+        //OdometryDatasetExtractorBuilder {
+            //velodyne_archive: None,            
+        //}
+    //}
+
+    ///// Iterates over every Velodyne capture available.
+    /////
+    ///// This function can thus be thought of as a fairly rough data-read primitive,
+    ///// likely requiring additional work to acheive more specific goals. To make
+    ///// this easy, this function makes a few garuntees:
+    /////
+    /////   1.) All captures for the current sequence will be yielded before captures from any other
+    /////       sequence.
+    /////   2.) Captures will be yielded in ascending order by their capture number.
+    /////
+    ///// This effectively means that the data returned is sorted, allowing for a larger set of
+    ///// processing operations to take place (like `filter()`ing).
+    /////
+    ///// Each capture likely will be quite large in size, so be cautious around `collect()`ing these
+    ///// data into an in-memory datastructure like `Vec`!
+    //pub fn captures(&mut self) -> impl Iterator<Item = OdometryCapture> + '_ {
+        //// Sort metadata
+        //let mut metadata: Vec<(u32, u32, usize)> = (0..self.velodyne_archive.len())
+            //.filter_map(|idx| {
+                //let file = self.velodyne_archive
+                    //.by_index(idx)
+                    //.expect("Failed to look up file.");
+                //let filename = file.name();
+
+                //let cap = match ODOMETRY_VELODYNE_FNAME_META_EXTACTOR.captures(filename) {
+                    //Some(cap) => cap,
+                    //None => return None,
+                //};
+                //let file_index = idx.clone();
+                //let sequence_num = &cap["sequence_num"].parse::<u32>().unwrap();
+                //let capture_num = &cap["capture_num"].parse::<u32>().unwrap();
+
+                //Some((*sequence_num, *capture_num, file_index))
+            //})
+            //.collect();
+        //log::debug!("Parsed velodyne archive metadata.");
+
+        //metadata.sort_by(|a, b| {
+            //let ord = a.0.cmp(&b.0);
+            //if ord != Ordering::Equal {
+                //return ord;
+            //}
+
+            //a.1.cmp(&b.1)
+        //});
+        //log::debug!("Sorted velodyne archive metadata.");
+
+        //metadata.into_iter()
+            //.map(move |(sequence_num, capture_num, archive_file_index)| {
+                //let mut file = self.velodyne_archive
+                    //.by_index(archive_file_index)
+                    //.unwrap();
+
+                //let mut points = Vec::new();
+                //loop {
+                    //let mut chunk = [0u8; 16];
+                    //if file.read(&mut chunk).unwrap() < 16 {
+                        //break;
+                    //}
+
+                    //let x = f32::from_ne_bytes(chunk[0..4].try_into().unwrap());
+                    //let y = f32::from_ne_bytes(chunk[4..8].try_into().unwrap());
+                    //let z = f32::from_ne_bytes(chunk[8..12].try_into().unwrap());
+                    //let _ = f32::from_ne_bytes(chunk[12..16].try_into().unwrap());
+
+                    //points.push(na::Vector3::new(x, y, z));
+                //}
+
+                //OdometryCapture {
+                    //sequence_num,
+                    //capture_num,
+                    //points,
+                //}
+            //})
+    //}
+//}
+
 #[derive(Debug)]
-pub struct OdometryCapture {
-    pub sequence_num: u32,
-    pub capture_num: u32,
-    pub points: Vec<na::Vector3<f32>>,  // TODO: determine the coordinate system used by kitti
+pub struct RawDatasetFrame {
+    timestamp: DateTime<Utc>,
+    image: PathBuf,
 }
 
-/// The  total kitti dataset is over 100gb, so I wanted to ensure this extractor could work w/ a
-/// partial copy. Multiple optional parameters seems to be best addressed with a builder, so here
-/// it is.
-pub struct OdometryDatasetExtractorBuilder {
-    velodyne_archive: Option<ZipArchive<BufReader<File>>>,
+pub struct RawDatasetExtractor {
+    timestamps: Vec<DateTime<Utc>>,
+    images: Vec<PathBuf>,
 }
 
-impl OdometryDatasetExtractorBuilder {
-    pub fn velodyne_zip(mut self, filepath: &str) -> Self {
-        let file = File::open(filepath).expect("Unable to locate velodyne archive");
-        let file_reader = BufReader::new(file);
+impl RawDatasetExtractor {
+    pub fn new(base_path: PathBuf)-> Self {
+        let mut timestamps_path = base_path.clone();
+        timestamps_path.push("image_00/timestamps.txt");
 
-        self.velodyne_archive = match ZipArchive::new(file_reader) {
-            Ok(a) => Some(a),
-            _ => {
-                log::warn!("Couldn't open provided velodyne archive @ {}",
-                    filepath);
-                None
-            }
-        };
-
-        self
-    }
-
-    pub fn build(self) -> OdometryDatasetExtractor {
-        OdometryDatasetExtractor {
-            velodyne_archive: self.velodyne_archive.expect("Couldn't find velodyne file!"),
-        }
-    }
-}
-
-pub struct OdometryDatasetExtractor {
-    velodyne_archive: ZipArchive<BufReader<File>>,
-}
-
-impl OdometryDatasetExtractor {
-    pub fn construct() -> OdometryDatasetExtractorBuilder {
-        OdometryDatasetExtractorBuilder {
-            velodyne_archive: None,            
-        }
-    }
-
-    /// Iterates over every Velodyne capture available.
-    ///
-    /// This function can thus be thought of as a fairly rough data-read primitive,
-    /// likely requiring additional work to acheive more specific goals. To make
-    /// this easy, this function makes a few garuntees:
-    ///
-    ///   1.) All captures for the current sequence will be yielded before captures from any other
-    ///       sequence.
-    ///   2.) Captures will be yielded in ascending order by their capture number.
-    ///
-    /// This effectively means that the data returned is sorted, allowing for a larger set of
-    /// processing operations to take place (like `filter()`ing).
-    ///
-    /// Each capture likely will be quite large in size, so be cautious around `collect()`ing these
-    /// data into an in-memory datastructure like `Vec`!
-    pub fn captures(&mut self) -> impl Iterator<Item = OdometryCapture> + '_ {
-        // Sort metadata
-        let mut metadata: Vec<(u32, u32, usize)> = (0..self.velodyne_archive.len())
-            .filter_map(|idx| {
-                let file = self.velodyne_archive
-                    .by_index(idx)
-                    .expect("Failed to look up file.");
-                let filename = file.name();
-
-                let cap = match ODOMETRY_VELODYNE_FNAME_META_EXTACTOR.captures(filename) {
-                    Some(cap) => cap,
-                    None => return None,
-                };
-                let file_index = idx.clone();
-                let sequence_num = &cap["sequence_num"].parse::<u32>().unwrap();
-                let capture_num = &cap["capture_num"].parse::<u32>().unwrap();
-
-                Some((*sequence_num, *capture_num, file_index))
+        let f = fs::File::open(timestamps_path).unwrap();
+        let timestamps = BufReader::new(f)
+            .lines()
+            .filter_map(|line| {
+                match line {
+                    Ok(line) => Some(line),
+                    _ => None,
+                }
             })
+            .map(|line| Utc.datetime_from_str(line.as_str(), "%Y-%m-%d %H:%M:%S%.f").unwrap())
             .collect();
-        log::debug!("Parsed velodyne archive metadata.");
 
-        metadata.sort_by(|a, b| {
-            let ord = a.0.cmp(&b.0);
-            if ord != Ordering::Equal {
-                return ord;
-            }
+        let mut images_dir = base_path.clone();
+        images_dir.push("image_00/data");
 
-            a.1.cmp(&b.1)
-        });
-        log::debug!("Sorted velodyne archive metadata.");
+        let images = fs::read_dir(images_dir).unwrap()
+            .map(|res| res.map(|e| e.path()).unwrap())
+            .collect();
 
-        metadata.into_iter()
-            .map(move |(sequence_num, capture_num, archive_file_index)| {
-                let mut file = self.velodyne_archive
-                    .by_index(archive_file_index)
-                    .unwrap();
+        Self {
+            timestamps,
+            images,
+        }
+    }
 
-                let mut points = Vec::new();
-                loop {
-                    let mut chunk = [0u8; 16];
-                    if file.read(&mut chunk).unwrap() < 16 {
-                        break;
-                    }
-
-                    let x = f32::from_ne_bytes(chunk[0..4].try_into().unwrap());
-                    let y = f32::from_ne_bytes(chunk[4..8].try_into().unwrap());
-                    let z = f32::from_ne_bytes(chunk[8..12].try_into().unwrap());
-                    let _ = f32::from_ne_bytes(chunk[12..16].try_into().unwrap());
-
-                    points.push(na::Vector3::new(x, y, z));
-                }
-
-                OdometryCapture {
-                    sequence_num,
-                    capture_num,
-                    points,
-                }
+    pub fn frames(&self) -> impl Iterator<Item=RawDatasetFrame> + '_ {
+        self.timestamps.iter()
+            .zip(self.images.iter())
+            .map(|(timestamp, image)| RawDatasetFrame { 
+                timestamp: timestamp.clone(),
+                image: image.clone(),
             })
     }
 }
--- random/rust/bevy/src/bin/helloworld.rs
+++ random/rust/bevy/src/bin/helloworld.rs
@@ -1,11 +1,56 @@
 use bevy::prelude::*;
 
+use malicious::kitti;
+
 fn hello_world() {
     println!("Hello world.");
 }
 
+fn extract_dataset() {
+    let mut path = home::home_dir().unwrap();
+    path.push("Datasets/kitti/raw/");
+
+    let dataset = kitti::RawDatasetExtractor::new(path);
+    //let frames = dataset.frames().collect::<Vec<kitti::RawDatasetFrame>>();
+    for frame in dataset.frames() {
+        println!("{:?}", frame);
+    }
+}
+
 fn main() {
     App::new()
-        .add_system(hello_world)
+        //.insert_resource(Msaa { samples: 4 })
+        //.add_plugins(DefaultPlugins)
+        .add_startup_system(hello_world)
+        .add_startup_system(extract_dataset)
+        //.add_startup_system(setup)
         .run();
+}
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    // set up the camera
+    let mut camera = OrthographicCameraBundle::new_3d();
+    camera.orthographic_projection.scale = 3.0;
+    camera.transform = Transform::from_xyz(5.0, 5.0, 5.0).looking_at(Vec3::ZERO, Vec3::Y);
+
+    // camera
+    commands.spawn_bundle(camera);
+
+    // cubes
+    commands.spawn_bundle(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
+        material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
+        transform: Transform::from_xyz(1.5, 0.5, 1.5),
+        ..Default::default()
+    });
+
+    // light
+    commands.spawn_bundle(PointLightBundle {
+        transform: Transform::from_xyz(3.0, 8.0, 5.0),
+        ..Default::default()
+    });
 }
