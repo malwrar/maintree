c0564ce5a4bc739fa6844f77f86e8a56675788cf
malwrar <malwrar@gmail.com>
2022-05-13 08:49:43 -0700

    Experiment with initial value, more complex function.

--- random/cpp/ceres-solver/src/helloworld.cpp
+++ random/cpp/ceres-solver/src/helloworld.cpp
@@ -5,6 +5,19 @@ using ceres::CostFunction;
 using ceres::Problem;
 using ceres::Solve;
 using ceres::Solver;
+
+//// A templated cost functor that implements the residual r = 10 -
+//// x. The method operator() is templated so that we can then use an
+//// automatic differentiation wrapper around it to generate its
+//// derivatives.
+//struct CostFunctor {
+//  template <typename T>
+//  bool operator()(const T* const x, T* residual) const {
+//    residual[0] = 10.0 - x[0];
+//    return true;
+//  }
+//};
+
 // A templated cost functor that implements the residual r = 10 -
 // x. The method operator() is templated so that we can then use an
 // automatic differentiation wrapper around it to generate its
@@ -12,23 +25,28 @@ using ceres::Solver;
 struct CostFunctor {
   template <typename T>
   bool operator()(const T* const x, T* residual) const {
-    residual[0] = 10.0 - x[0];
+    residual[0] = 0.5 * ((10.0 - x[0]) * (10.0 - x[0]));
     return true;
   }
 };
+
 int main(int argc, char** argv) {
   google::InitGoogleLogging(argv[0]);
+
   // The variable to solve for with its initial value. It will be
   // mutated in place by the solver.
-  double x = 0.5;
+  double x = 1.0;
   const double initial_x = x;
+
   // Build the problem.
   Problem problem;
+
   // Set up the only cost function (also known as residual). This uses
   // auto-differentiation to obtain the derivative (jacobian).
   CostFunction* cost_function =
       new AutoDiffCostFunction<CostFunctor, 1, 1>(new CostFunctor);
   problem.AddResidualBlock(cost_function, nullptr, &x);
+
   // Run the solver!
   Solver::Options options;
   options.minimizer_progress_to_stdout = true;
