d8e84f3ccafb7822101b5a910652541de1a304c0
malwrar <malwrar@gmail.com>
2022-05-21 17:13:43 -0700

    Add timing debug print.

--- random/cpp/vision/src/featurestuff.cpp
+++ random/cpp/vision/src/featurestuff.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+#include <chrono>
 #include <set>
 #include <vector>
 
@@ -9,6 +10,21 @@
 static cv::Ptr<cv::Feature2D> AKAZE = cv::AKAZE::create();
 static cv::Ptr<cv::CLAHE> CLAHE = cv::createCLAHE();
 
+#define DEBUG
+#ifdef DEBUG
+#define PRINT_DEBUG(...) do{ fprintf( stderr, __VA_ARGS__ ); } while( false )
+#else
+#define PRINT_DEBUG(...) do{ } while ( false )
+#endif
+
+//class Keypoint {
+//public:
+//    Keypoint(int x, int y): 2d_x(x), 2d_y(y) {}
+//
+//private:
+//    int 2d_x, 2d_y;
+//}
+
 void draw_keypoints(
     cv::Mat& image,
     const std::vector<cv::KeyPoint>& keypoints
@@ -16,10 +32,10 @@ void draw_keypoints(
     for (size_t i=0; i < keypoints.size(); i++) {
         int x = keypoints[i].pt.x + 0.5;
         int y = keypoints[i].pt.y + 0.5;
-	float radius = keypoints[i].size / 2.0;
+	    float radius = keypoints[i].size / 2.0;
 
-	cv::circle(image, cv::Point(x, y), 2.5 * radius, cv::Scalar(0, 255, 0), 1);
-	cv::circle(image, cv::Point(x, y), 1.0, cv::Scalar(0, 0, 255), -1);
+	    cv::circle(image, cv::Point(x, y), 2.5 * radius, cv::Scalar(0, 255, 0), 1);
+	    cv::circle(image, cv::Point(x, y), 1.0, cv::Scalar(0, 0, 255), -1);
     }
 }
 
@@ -32,25 +48,35 @@ int main(int argc, char** argv) {
     }
 
     while (true) {
+        // Exit if esc or q key is pressed
+	    int key = cv::waitKey(1);
+	    if (key == 'q' || key == 27 /* ESC */) {
+            break;
+	    }
+
         cv::Mat image;
         camera >> image;  // I really hate this syntax
 
         //cv::Mat image_contrast_enhanced;
-	//printf("%u %u\n", image.type(), image_contrast_enhanced.type());
-	//CLAHE->apply(image, image_contrast_enhanced);
+	    //printf("%u %u\n", image.type(), image_contrast_enhanced.type());
+	    //CLAHE->apply(image, image_contrast_enhanced);
 
-	std::vector<cv::KeyPoint> keypoints;
+        auto start = std::chrono::steady_clock::now();
+
+        std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+
+	    std::vector<cv::KeyPoint> keypoints;
         cv::Mat descriptors;
-	AKAZE->detectAndCompute(image, cv::noArray(), keypoints, descriptors,
-                false);
-	draw_keypoints(image, keypoints);
+	    AKAZE->detectAndCompute(image, cv::noArray(), keypoints, descriptors,
+                    false);
 
+        std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+        
+        PRINT_DEBUG("feature detection time = %.4f\n",
+                (float)(std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count()) / 1000.0f);
+
+	    draw_keypoints(image, keypoints);
         cv::imshow("webcam", image);
-
-	int key = cv::waitKey(1);
-	if (key == 'q' || key == 27 /* ESC */) {
-            break;
-	}
     }
 
     return 0;
