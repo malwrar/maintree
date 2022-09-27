a63b17d189f66082f276b0c866d051dc2f461ddb
malwrar <malwrar@gmail.com>
2022-09-26 18:46:59 -0700

    Remove unused axis draw fn, opencv imports.

--- random/rust/vision/src/tracking.rs
+++ random/rust/vision/src/tracking.rs
@@ -31,52 +31,6 @@ use opencv::{
     videoio,
 };
 
-fn draw_axis(image: &mut Mat, r: &Mat, t: &Mat, k: &Mat, dist_coeffs: &Mat)  -> Result<()> {
-	// Define the virual 3d axis lines.
-	let points = VectorOfPoint3f::from_slice(&[
-		Point3f::new(1.0, 0.0, 0.0),  // [0]: x
-		Point3f::new(0.0, 1.0, 0.0),  // [1]: y
-		Point3f::new(0.0, 0.0, 1.0),  // [2]: z
-		Point3f::new(0.0, 0.0, 0.0),  // [3]: origin
-	]);
-
-	let mut image_points = VectorOfPoint2f::new();
-	let mut jacobian = Mat::default();
-	calib3d::project_points(&points, &r, &t, &k, dist_coeffs,
-			&mut image_points, &mut jacobian, 0.0)
-		.unwrap();
-	// TODO: inverse of PnP?? Make a test program with artificial data.
-
-	// Palette
-	let r = Scalar::new(255.0, 0.0, 0.0, 1.0);
-	let g = Scalar::new(0.0, 255.0, 0.0, 1.0);
-	let b = Scalar::new(0.0, 0.0, 255.0, 1.0);
-	let c = Scalar::new(255.0, 255.0, 0.0, 1.0);
-
-	// Points
-	let image_points = image_points.as_slice();
-	let p0 = image_points[0];
-	let p1 = image_points[1];
-	let p2 = image_points[2];
-	let p3 = image_points[3];
-
-	let x = Point2i::new(p0.x as i32, p0.y as i32);
-	let y = Point2i::new(p1.x as i32, p1.y as i32);
-	let z = Point2i::new(p2.x as i32, p2.y as i32);
-	let origin = Point2i::new(p3.x as i32, p3.y as i32);
-
-	imgproc::line(image, origin, x, r, 3, 0, 0).unwrap();
-	imgproc::line(image, origin, y, g, 3, 0, 0).unwrap();
-	imgproc::line(image, origin, z, b, 3, 0, 0).unwrap();
-	
-	// Draw the 2d points for good measure.
-	//for p in image_points {
-	//	imgproc::circle(image, Point2i::new(p.x as i32, p.y as i32), 1, c, 3, 0, 0).unwrap();
-	//}
-
-	Ok(())
-}
-
 fn preprocess_image(image: &Mat) -> Mat {
 	let mut gray = Mat::default();
 	imgproc::cvt_color(&image, &mut gray, imgproc::COLOR_BGR2GRAY, 0).unwrap();
