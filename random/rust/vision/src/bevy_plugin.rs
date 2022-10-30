da0173e9a00da414441a06b484ec619142345e6c
malwrar <malwrar@gmail.com>
2022-10-30 14:49:09 -0700

    Add observer tracking and 3d scene rendering.

--- random/rust/vision/src/bevy_plugin.rs
+++ random/rust/vision/src/bevy_plugin.rs
@@ -1,11 +1,32 @@
-use rand::Rng;
-
 use bevy::{
     prelude::*,
     app::Plugin,
 };
 
 use crossbeam_channel::{bounded, Receiver};
+
+use opencv::{
+    calib3d,
+    core::{
+        Point2i,
+        Point3f,
+        Scalar,
+    },
+    highgui,
+    imgproc,
+    prelude::*,
+    types::{
+        VectorOfPoint3f,
+        VectorOfPoint2f,
+    },
+    videoio,
+    Result,
+};
+
+use crate::{
+    calibration::CameraCalibration,
+    tracking::Tracker,
+};
 
 #[derive(Default)]
 pub struct CvPlugin;
@@ -50,17 +71,67 @@ fn setup_processing_thread(mut commands: Commands) {
     let (tx, rx) = bounded::<CvEvent>(1);
 
     std::thread::spawn(move || {
+        // Setup debug window that shows input footage.
+        let window = "Observer playback";
+        highgui::named_window(window, highgui::WINDOW_NORMAL)
+            .expect("Failed to create debug window!");
+
+        // When footage ends, repeat playback.
         loop {
-            let pose = Pose {
-                translation: Vec3::new(0.0,
-                    rand::thread_rng().gen_range(1.0..10.0),
-                    rand::thread_rng().gen_range(1.0..10.0)),
-                ..Default::default()
-            };
+            // Open input video feed & metadata.
+            let mut file = videoio::VideoCapture::from_file("./assets/office_calib_iphone/orbit_left_right.mov", videoio::CAP_ANY)
+                .expect("Failed to open video file.");
 
-            tx.send(CvEvent::ViewerPoseUpdate(pose)).unwrap();
+            let calib = CameraCalibration::from_file(String::from("./assets/office_calib_iphone/orbit_left_right.yaml"))
+                .expect("Failed to open calibration file.");
 
-            std::thread::sleep(std::time::Duration::from_secs(1));
+            if !videoio::VideoCapture::is_opened(&file)
+                   .expect("Failed to open file.") {
+                panic!("Unable to open file!");
+            }
+
+            // Setup cv resources.
+            let tracker = Tracker::new(calib);
+
+            loop {
+                if highgui::wait_key(10).expect("") > 0 { break; }
+
+                // Read next frame.
+                let mut frame = Mat::default();
+                if !file.read(&mut frame).expect("Failed to read next frame.") {
+                    break;
+                }
+
+                // Try to (re)locate points, lines, & regions, use these to
+                // estimate the observer's pose.
+                let mut rvec = Mat::default();
+                let mut tvec = Mat::default();
+
+                if tracker.track(&frame, &mut rvec, &mut tvec) {
+                    let translation = Vec3::new(
+                            *tvec.at_2d::<f64>(0, 0).unwrap() as f32,
+                            *tvec.at_2d::<f64>(1, 0).unwrap() as f32,
+                            *tvec.at_2d::<f64>(2, 0).unwrap() as f32);
+
+                    let pose = Pose {
+                        translation,
+                        ..Default::default()
+                    };
+
+                    // Send pose update to the renderer.
+                    tx.send(CvEvent::ViewerPoseUpdate(pose)).unwrap();
+                }
+
+                // HACK: shrink window while retaining aspect ratio.
+                let aspect_ratio = frame.rows() as f32 / frame.cols() as f32;
+                let width = 600.0;
+                highgui::resize_window(window, width as i32,
+                        (width * aspect_ratio) as i32)
+                    .unwrap();
+
+                highgui::imshow(window, &frame)
+                    .unwrap();
+            }
         }
     });
 
--- random/rust/vision/src/bin/renderer.rs
+++ random/rust/vision/src/bin/renderer.rs
@@ -1,137 +1,52 @@
 //! 3d renderer for 
 
-use rand::Rng;
-
 use bevy::{
     prelude::*,
-    render::camera::Projection,
     window::PresentMode,
 };
 
-use crossbeam_channel::{bounded, Receiver};
-
-use opencv::{
-	highgui,
-	prelude::*,
-	videoio,
-};
-
 use vision::{
-    calibration::CameraCalibration,
-    tracking::Tracker,
+    bevy_plugin::{CvPlugin, CvEvent},
 };
 
-#[derive(Clone, Copy, Debug)]
-struct Pose {
-    translation: Vec3,
-    rotation: Vec3,
+use bevy_sandbox::{
+    debug::DebugPlugin,
+    debug_camera::{
+        CameraController,
+        CameraControllerPlugin
+    },
+};
+
+fn main() {
+    App::new()
+        .insert_resource(Msaa { samples: 4 })
+        .insert_resource(WindowDescriptor {
+            title: "3d Visualization".to_string(),
+            width: 600.,
+            height: 600.,
+            present_mode: PresentMode::AutoVsync,
+            ..default()
+        })
+        .add_plugins(DefaultPlugins)
+        .add_plugin(CameraControllerPlugin)
+        .add_plugin(CvPlugin)
+        .add_plugin(DebugPlugin)
+        .add_startup_system(setup_scene)
+        .add_system(update)
+        .run();
 }
 
-impl Default for Pose {
-    fn default() -> Self {
-        Self { 
-            translation: Vec3::new(0.0, 0.0, 0.0),
-            rotation: Vec3::new(0.0, 0.0, 0.0),
-        }
-    }
-}
+#[derive(Component, Default)]
+struct Observer;
 
-#[derive(Deref)]
-struct PoseReceiver(Receiver<Pose>);
-struct PoseEvent(Pose);
+#[derive(Component, Default)]
+struct Object;
 
-#[derive(Component)]
-struct ARCamera {}
-
-fn setup_tracker(mut commands: Commands) {
-    let (tx, rx) = bounded::<Pose>(1);
-
-    std::thread::spawn(move || {
-    	let window = "video capture";
-    	highgui::named_window(window, 1)
-            .expect("Failed to create debug window!");
-        
-        let mut file = videoio::VideoCapture::from_file("./assets/tracking_test_1.mp4", videoio::CAP_ANY)
-            .expect("Failed to open video file.");
-        
-        let calib = CameraCalibration::from_file(String::from("./assets/tracking_test_1.calib.yaml"))
-            .expect("Failed to open calibration file.");
-        
-    	if !videoio::VideoCapture::is_opened(&file)
-                .expect("Failed to open file.") {
-    		panic!("Unable to open file!");
-    	}
-    
-        let tracker = Tracker::new(calib);
-
-    	loop {
-    		if highgui::wait_key(10).expect("") > 0 { break; }
-
-    		let mut frame = Mat::default();
-    		if !file.read(&mut frame).expect("Failed to read next frame.") {
-                break;
-            }
-        
-            let mut tvec = Mat::default();
-            let mut rvec = Mat::default();
-
-            if tracker.track(&mut frame, &mut tvec, &mut rvec) {
-
-                let translation = Vec3::new(
-                        *tvec.at_2d::<f64>(0, 0).unwrap() as f32,
-                        *tvec.at_2d::<f64>(1, 0).unwrap() as f32,
-                        *tvec.at_2d::<f64>(2, 0).unwrap() as f32);
-
-                let rotation = Vec3::new(
-                        (*tvec.at_2d::<f64>(0, 0).unwrap() as f32).to_radians(),
-                        (*tvec.at_2d::<f64>(1, 0).unwrap() as f32).to_radians(),
-                        (*tvec.at_2d::<f64>(2, 0).unwrap() as f32).to_radians());
-
-                tx.send(Pose {
-                        translation,
-                        rotation,
-                    })
-                    .expect("Failed to send pose update.");
-            }
-        
-
-            highgui::imshow(window, &frame)
-                .expect("Failed to show frame on debug window!");
-        }
-    });
-
-    commands.insert_resource(PoseReceiver(rx));
-}
-
-fn read_from_tracker(
-    receiver: ResMut<PoseReceiver>,
-    mut events: EventWriter<PoseEvent>
-) {
-    for pose in receiver.try_iter() {
-        events.send(PoseEvent(pose));
-    }
-}
-
-fn setup(
+fn setup_scene(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
-    // plane
-    commands.spawn_bundle(PbrBundle {
-        mesh: meshes.add(Mesh::from(shape::Plane { size: 50.0 })),
-        material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
-        ..default()
-    });
-
-    // cube
-    commands.spawn_bundle(PbrBundle {
-        mesh: meshes.add(Mesh::from(shape::Cube { size: 3.0 })),
-        material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
-        transform: Transform::from_xyz(0.0, 0.5, 0.0),
-        ..default()
-    });
-
     // light
     commands.spawn_bundle(PointLightBundle {
         point_light: PointLight {
@@ -139,55 +54,48 @@ fn setup(
             shadows_enabled: true,
             ..default()
         },
-        transform: Transform::from_xyz(4.0, 8.0, 4.0),
+        transform: Transform::from_xyz(0.0, 20.0, 0.0),
         ..default()
     });
 
     // camera
-    let projection = Projection::default();
-
     commands.spawn_bundle(Camera3dBundle {
-        transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
-        projection,
+        transform: Transform::from_xyz(25.0, 10.0, 0.0)
+            .looking_at(Vec3::ZERO, Vec3::Y),
         ..default()
-    }).insert(ARCamera {
+    })
+    .insert(CameraController::default());
 
-    });
+
+    // cube
+    commands.spawn_bundle(PbrBundle {
+        transform: Transform::from_xyz(0.0, 0.0, 0.0),
+        mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
+        material: materials.add(Color::rgb(1.0, 0.0, 0.0).into()),
+        ..default()
+    })
+    .insert(Observer);
 }
 
 fn update(
-    mut pose_updates: EventReader<PoseEvent>,
-    mut query: Query<(&mut ARCamera, &mut Transform)>,
+    mut cv_events: EventReader<CvEvent>,
+    mut query: Query<(&mut CameraController, &mut Transform)>,
 ) {
-    for e in pose_updates.iter() {
-        let pose = e.0;
+    for e in cv_events.iter() {
 
-        println!("Updating pose {:?}", pose);
+        println!("Updating pose {:?}", e);
 
-        for (_camera, mut transform) in query.iter_mut() {
-            *transform = transform
-                .with_translation(pose.translation)
-                .with_rotation(Quat::from_scaled_axis(pose.rotation));
-                //.looking_at(Vec3::ZERO, Vec3::Y);
+        let (_, mut transform) = query.iter_mut().last().unwrap();
+
+        match e {
+            CvEvent::ViewerPoseUpdate(pose) => {
+                let t = pose.translation;
+                let t = Vec3::new(t.x, t.y, -t.z);
+
+                *transform = transform
+                    .with_translation(t);
+            },
+            //e => println!("Unhandled CvEvent: {:?}", e),
         }
     }
-}
-
-fn main() {
-    App::new()
-        .insert_resource(Msaa { samples: 4 })
-        .insert_resource(WindowDescriptor {
-            title: "Tracked cube position".to_string(),
-            width: 600.,
-            height: 600.,
-            present_mode: PresentMode::AutoVsync,
-            ..default()
-        })
-        .add_event::<PoseEvent>()
-        .add_plugins(DefaultPlugins)
-        .add_startup_system(setup)
-        .add_startup_system(setup_tracker)
-        .add_system(read_from_tracker)
-        .add_system(update)
-        .run();
 }
